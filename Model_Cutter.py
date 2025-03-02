8724e35b7b58ec1d144d36beb189fc2c929efd7a
crane206265 <107654183+crane206265@users.noreply.github.com>
2025-03-03 03:27:24 +0900

    Update Model_Cutter.py

--- Model_Cutter.py
+++ Model_Cutter.py
@@ -1,37 +1,54 @@
 import numpy as np
+from numpy import linalg as LA
 
 class CutterSphere():
-    def __init__(self, ast, random = True, *args):
+    def __init__(self, ast, random = True, mode = 'Rxyz_assign', *args):
         """
         initialize
         - if random == True, use random parameters
         - if random == False, use parameters from *args
         *args = (R, x1, y1, z1)
+            if mode == 'ratio_assign' *args = (phi, theta, r_cen_ratio, r_cut_ratio)
         ast : Asteroid_Model#class
         """
-        if random == False:
-            self.radi = args[0]
-            self.x1 = args[1]
-            self.y1 = args[2]
-            self.z1 = args[3]
-            return
-        
         self.k = 0.2 #cut ratio 0.2
         self.min_cen = 3 #3
         self.max_cen = 10
 
-        self.phi_cen = 2*np.pi*np.random.rand()
-        self.theta_cen = np.pi*np.random.rand()
+        if random == False and mode == 'Rxyz_assign':
+            self.radi = args[0]
+            self.x1 = args[1]
+            self.y1 = args[2]
+            self.z1 = args[3]
+
+            self.r_cen, self.phi_cen, self.theta_cen = CutterSphere.__cart2sph((self.x1, self.y1, self.z1))
+            return
+        
+        elif random == False and mode == 'ratio_assign':
+            self.phi_cen = 2*np.pi*args[0]
+            self.theta_cen = np.pi*args[1]
+            self.r_cen_ratio = args[2]
+            self.R_cut_ratio = args[3]
+        
+        else:
+            self.phi_cen = 2*np.pi*np.random.rand()
+            self.theta_cen = np.pi*np.random.rand()
+
         self.j_cen = round(self.theta_cen/ast.dtheta)
         if self.j_cen%2 == 0:
             self.i_cen = round(self.phi_cen/ast.dphi)
         else:
             self.i_cen = round((self.phi_cen-ast.dphi/2)/ast.dphi)
-
         self.r_ast = ast.pos_sph_arr[self.i_cen, self.j_cen, 0]
-        self.r_cen = (self.min_cen + (self.max_cen-self.min_cen)*np.random.rand())*self.r_ast
+
+        if random == False and mode == 'ratio_assign':
+            self.r_cen = (self.min_cen + (self.max_cen-self.min_cen)*self.r_cen_ratio)*self.r_ast
+            self.radi = self.k*self.r_ast*self.R_cut_ratio + self.r_cen - self.r_ast
         
-        self.radi = self.k*self.r_ast*np.random.rand() + self.r_cen - self.r_ast
+        else:
+            self.r_cen = (self.min_cen + (self.max_cen-self.min_cen)*np.random.rand())*self.r_ast
+            self.radi = self.k*self.r_ast*np.random.rand() + self.r_cen - self.r_ast
+
         cart = ast.sph2cart([self.r_cen, self.phi_cen, self.theta_cen])
         self.x1 = cart[0]
         self.y1 = cart[1]
@@ -63,3 +80,19 @@ class CutterSphere():
 
         r_f = self.r_cen*cosa - ((self.r_cen*cosa)**2 - (self.r_cen**2 - self.radi**2))**0.5
         return r_f
+    
+    @staticmethod
+    def __cart2sph(cart_coord):
+        """
+        cartesian coord -> spherical coord
+        input : cart_coord = (x, y, z)
+        output : (r, phi, theta)
+        """
+        x = cart_coord[0]
+        y = cart_coord[1]
+        z = cart_coord[2]
+        r = LA.norm(np.array([x, y, z]))
+        phi = np.arctan2(y, x)
+        theta = np.arccos(z/r)
+        
+        return r, phi, theta
